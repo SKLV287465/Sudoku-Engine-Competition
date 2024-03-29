01748dca63ac7010608f58e40562f2416c1ad0a4
Joshua Shim <sklave@Joshuas-MacBook-Pro.local>
2024-03-29 19:27:18 +1100

    finished setup

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import collections
 
 def is_complete(board):
     for x in range(9):
@@ -9,11 +10,23 @@ def is_complete(board):
 
 def is_correct(board):
     # check every row
+    for i in range(9):
+        row = [0] * 9
+        column = [0] * 9
+        for j in range(9):
+            row[board[i][j]] += 1
+            column[board[j][i]] += 1
+        for j in range(9):
+            if row[j] != 1 or column[j] != 1:
+                return False
+    squares = collections.defaultdict(set)
     
-    # check every column
-    
-    #check every box
-
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] in squares(i // 3, j // 3):
+                return False
+            squares(i // 3, j // 3).add(board[i][j])
+    return True
 
 board = [[0] * 9] * 9
 
@@ -21,6 +34,14 @@ board = [[0] * 9] * 9
 for x in range(1, len(sys.argv)):
     group = sys.argv[x]
     board[int(sys.argv[x][0])][int(sys.argv[x][1])] = int(sys.argv[x][2])
+    
+    # run function 
+    
+    if is_correct(board) and is_complete(board):
+        print('Sudoku successfully completed')
+        exit
+    print('You are stupid')
+    
 
 
         
