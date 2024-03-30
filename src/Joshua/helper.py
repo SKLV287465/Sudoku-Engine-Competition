432e3c7e5205859de645ea8fba123993a662fd11
Joshua Shim <sklave@Joshuas-MacBook-Pro.local>
2024-03-30 11:33:39 +1100

    recommit

--- src/Joshua/helper.py
+++ src/Joshua/helper.py
@@ -31,7 +31,34 @@ def valid_box(number, x, y, board):
 
 # check that it is only valid in row
 def only_valid_in_row(number, x, y, board):
+    if not valid_box(number, x, y, board) or not valid_row(number, x, y, board):
+        return False
+    for i in range(9):
+        if i == y:
+            continue
+        if valid_col(board[x][i]):
+            return False
+    return True
 # check that it is only valid in col
-def only_valid_in_row(number, x, y, board):
+def only_valid_in_col(number, x, y, board):
+    if not valid_box(number, x, y, board) or not valid_col(number, x, y, board):
+        return False
+    for i in range(9):
+        if i == x:
+            continue
+        if valid_row(board[i][y]):
+            return False
+    return True
 # check that it is only valid in box
-def only_valid_in_row(number, x, y, board):
+def only_valid_in_box(number, x, y, board):
+    if not valid_box(number, x, y, board):
+        return False
+    bx = x // 3
+    by = y // 3
+    for i in range(3 * bx, 3 * bx + 3):
+        for j in range(3 * by, 3 * by + 3):
+            if y == j and x == i:
+                continue
+            if valid_col(board[i][j]) and valid_row(board[i][j]):
+                return False
+    return True
--- src/Joshua/solution.py
+++ src/Joshua/solution.py
@@ -1,6 +1,9 @@
 import helper as h
+import src.main as m
+import guess as g
+import threading as th
 
-def solve(board):
+def simple_solve(board):
     changes = 1
     while changes:
         changes = 0
@@ -10,4 +13,22 @@ def solve(board):
                     if h.only_valid_in_box(n, x, y, board) or h.only_valid_in_col(n, x, y, board) or h.only_valid_in_row(n, x, y, board):
                         board[x][y] = n
                         changes += 1
+    completed = False
+    correct = False
+    if m.is_complete(board):
+        completed = True
+    if m.is_correct(board):
+        correct = True
+    return board, completed, correct
+
+def solve(board):
+    board, done, solved = simple_solve(board)
+    if solved and done: 
+        return board
+    binaries = g.get_binaries(board)
+    correct_binary = [0] * len(binaries)
+    for i in range(len(binaries)):
+        new_board = board
+        new_board[binaries[1]][binaries[2]] = binaries[0]
+        
     return board
